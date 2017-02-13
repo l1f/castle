5d088c10345119cf16e3d9ec86dbbc7196189dab
piaomiao1314 <lifeng_3344@qq.com>
2017-02-14 02:18:59 +0800

    增加互逆方向，增加链式调用

--- src/castle/Game.java
+++ src/castle/Game.java
@@ -1,12 +1,13 @@
 package castle;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Game {
     private Room currentRoom;
-    private HashMap<String,Handler> handlers = new HashMap<String, Handler>();
-        
+    private HashMap<String, Handler> handlers = new HashMap<String, Handler>();
+    private HashMap<String, String> directionReverse = new HashMap<String, String>();
     public Game() 
     {
     		handlers.put("bye", new HandlerBye(this));
@@ -14,28 +15,46 @@ public class Game {
     		handlers.put("go", new HandlerGo(this));
         createRooms();
     }
-
+    
+    // 增加一对互逆方向
+    private Game addReverseDirection(String d1,String d2){
+    		// 如果d1或d2已定义了方向绑定，则先解除与其互逆方向字符串的绑定
+    		if(directionReverse.containsKey(d1))
+    			directionReverse.remove( directionReverse.get(d1) );
+    		if(directionReverse.containsKey(d2))
+    			directionReverse.remove( directionReverse.get(d2) );
+    		directionReverse.put(d1, d2);
+    		directionReverse.put(d2, d1);
+    		return this;
+    }
+    
+    // room1 的 direction 方向上是 room2， 如果这是一个已定义的互逆方向，则room2的相反方式上是room1
+    private Game setRoomExit(Room room1, String direction, Room room2){
+    		room1.setExit(direction, room2);
+    		if(directionReverse.containsKey(direction)){
+    			String reverse = directionReverse.get(direction);
+    			room2.setExit(reverse, room1);
+    		}
+    		return this;
+    }
+    
     private void createRooms()
     {
-        Room outside, lobby, pub, study, bedroom;
-      
+    		//  配置互逆方向
+    		this.addReverseDirection("left", "right")
+    			.addReverseDirection("top", "bottom")
+    			.addReverseDirection("up", "down");
         //	制造房间
-        outside = new Room("城堡外");
-        lobby = new Room("大堂");
-        pub = new Room("小酒吧");
-        study = new Room("书房");
-        bedroom = new Room("卧室");
+        Room roomA = new Room("A");
+        Room roomB = new Room("B");
+        Room roomC = new Room("C");
+        Room roomD = new Room("D");
         
-        outside.setExit("left", study);
-        //study.setExit("right", outside);
-        //	初始化房间的出口
-//        outside.setExits(null, lobby, study, pub);
-//        lobby.setExits(null, null, null, outside);
-//        pub.setExits(null, outside, null, null);
-//        study.setExits(outside, bedroom, null, null);
-//        bedroom.setExits(null, null, null, study);
-
-        currentRoom = outside;  //	从城堡门外开始
+        // 配置相对关系
+        this.setRoomExit(roomA, "right", roomB)
+        		.setRoomExit(roomB, "right", roomC)
+        		.setRoomExit(roomC, "top", roomD);
+        currentRoom = roomA;  //	从房间A开始
     }
 
     private void printWelcome() {
