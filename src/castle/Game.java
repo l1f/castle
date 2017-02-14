8e444796051db26d70ea9b35ddcbbb1039654180
l1f <294916781@qq.com>
2017-02-14 20:35:29 +0800

    显示全景地图，HashMap保存房间

--- src/castle/Game.java
+++ src/castle/Game.java
@@ -4,16 +4,29 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
+import com.sun.org.omg.CORBA.ExceptionDescription;
+
 public class Game {
 	private Room currentRoom;
 	private HashMap<String, Handler> handlers = new HashMap<String, Handler>();
 	private HashMap<String, String> directionReverse = new HashMap<String, String>();
+	private HashMap<String, Room> rooms = new HashMap<String, Room>();
+	private String mapStr = "";
 
 	public Game() {
 		handlers.put("bye", new HandlerBye(this));
 		handlers.put("help", new HandlerHelp(this));
 		handlers.put("go", new HandlerGo(this));
 		createRooms();
+	}
+
+	private Game setMapStr(String mapStr) {
+		this.mapStr = mapStr;
+		return this;
+	}
+
+	public String getMapStr() {
+		return mapStr;
 	}
 
 	// 增加一对互逆方向
@@ -29,7 +42,9 @@ public class Game {
 	}
 
 	// room1 的 direction 方向上是 room2， 如果这是一个已定义的互逆方向，则room2的相反方式上是room1
-	private Game setRoomExit(Room room1, String direction, Room room2) {
+	private Game setRoomExit(String room1Key, String direction, String room2Key) {
+		Room room1 = rooms.get(room1Key);
+		Room room2 = rooms.get(room2Key);
 		room1.setExit(direction, room2);
 		if (directionReverse.containsKey(direction)) {
 			String reverse = directionReverse.get(direction);
@@ -38,19 +53,55 @@ public class Game {
 		return this;
 	}
 
+	private Game setCurrentRoom(String roomKey) {
+		currentRoom = rooms.get(roomKey);
+		return this;
+	}
+
+	// 增加Game内Room
+	private Game addRoom(String roomKey, String roomName) {
+		rooms.put(roomKey, new Room(roomName));
+		return this;
+	}
+
 	private void createRooms() {
 		// 配置互逆方向
-		this.addReverseDirection("left", "right").addReverseDirection("top", "bottom").addReverseDirection("up",
-				"down");
-		// 制造房间
-		Room roomA = new Room("A");
-		Room roomB = new Room("B");
-		Room roomC = new Room("C");
-		Room roomD = new Room("D");
+		this.addReverseDirection("left", "right").addReverseDirection("top", "bottom").addReverseDirection("up", "down")
+				// 制造房间
+				.addRoom("A", "roomA").addRoom("B", "roomB").addRoom("C", "roomC").addRoom("D", "roomD")
+				.addRoom("E", "roomE").addRoom("F", "roomF").addRoom("G", "roomG").addRoom("H", "roomH")
+				.addRoom("O", "大厅").addRoom("loft", "阁楼")
 
-		// 配置相对关系
-		this.setRoomExit(roomA, "right", roomB).setRoomExit(roomB, "right", roomC).setRoomExit(roomC, "top", roomD);
-		currentRoom = roomA; // 从房间A开始
+				// 配置相对关系
+				.setRoomExit("A", "top", "B").setRoomExit("B", "right", "C").setRoomExit("C", "right", "D")
+				.setRoomExit("D", "bottom", "E").setRoomExit("E", "bottom", "F").setRoomExit("F", "left", "G")
+				.setRoomExit("G", "left", "H").setRoomExit("H", "up", "loft").setRoomExit("H", "top", "A")
+				.setRoomExit("O", "left", "A").setRoomExit("O", "right", "E")
+				// 从房间A开始
+				.setCurrentRoom("O");
+
+		// 设置地图字符串
+		StringBuffer sb = new StringBuffer();
+		sb.append("   +-----+-----+-----+               ^ top          \n")
+				.append("   |     |     |     |               |              \n")
+				.append("   |  B     C     D  |               |              \n")
+				.append("   |     |     |     |               |              \n")
+				.append("   +-- ----------- --+               |              \n")
+				.append("   |     |     |     |  left         |       right  \n")
+				.append("   |  A     O     E  |     <---------x---------->   \n")
+				.append("   |     |     |     |               |              \n")
+				.append("   +-- ----------- --+               |              \n")
+				.append("   +-+   |     |     |               |              \n")
+				.append("   |.|H     G     F  |               |              \n")
+				.append("   +-+   |     |     |               |              \n")
+				.append("   +-----+-----+-----+               v bottom       \n")
+				.append("                                                    \n")
+				.append("   +-----------+                                    \n")
+				.append("   +-+         |              +-+       +-+         \n")
+				.append("   |x|  loft   |              |x| down  |.| up      \n")
+				.append("   +-+         |              +-+       +-+         \n")
+				.append("   +-----------+                                    \n");
+		this.setMapStr(sb.toString());
 	}
 
 	private void printWelcome() {
@@ -94,6 +145,8 @@ public class Game {
 				handler.doCmd(cmd);
 				if (handler.isBye())
 					break;
+			} else {
+				System.out.println("命令错误 ╭(╯^╰)╮");
 			}
 		}
 		in.close();
--- src/castle/HandlerHelp.java
+++ src/castle/HandlerHelp.java
@@ -6,9 +6,17 @@ public class HandlerHelp extends Handler {
 	}
 
 	@Override
-	public void doCmd(String cmd) {
+	public void doCmd(String arg) {
 		System.out.print("迷路了吗？你可以做的命令有：go bye help");
-		System.out.println("如：\tgo east");
+		System.out.println("如：\tgo left");
+		if (game.getMapStr() != "") {
+			System.out.println("彩蛋:输入\"help map\",查看上帝视角全景地图");
+
+		}
+		if (arg.equals("map")) {
+			System.out.println("上帝视角全景地图如下:");
+			System.out.println(game.getMapStr());
+		}
 	}
 
 }
